bfe8f52156ba669991341cd17018e0353582be49
Florisvan der Breggen <florisvanderbreggen@gmail.com>
2018-03-06 16:50:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logshout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logwhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma() {
+  if (string != string.toUpperCase(string)) {
+    return `I can’t hear you!`
+  }
+  else if (string === string.toUpperCase()) {
+    return `YES INDEED!`;
+  }
+  else {
+    return `I love you, too.`;
+  }
+}
+
+sayHiToGrandma('yolooooOEOO')
